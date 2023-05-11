3896431b2214044af850e732242e9e78b7e834de
FBW1232 <fabianowelter3@gmail.com>
2023-05-11 16:14:31 -0300

    aaa

--- PE.h
+++ PE.h
@@ -5,17 +5,17 @@
 #include <string.h>
 #define MAX 1024
 
-typedef struct Pilha{
-    int vet[MAX];
-    Anime *topo;
-}pilha;
-
 typedef struct Anime{
     char nome[MAX];
     int f_download;
     int f_assistir;
     struct Anime *prox;
 }Anime;
+
+typedef struct Pilha{
+    int vet[MAX];
+    Anime *topo;
+}pilha;
 
 pilha* criaPilha();
 int p_vazia(pilha *p);
