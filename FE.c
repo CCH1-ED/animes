f3588c2bc99695dceb88fb98a84d4e1e34e1d34e
FBW1232 <fabianowelter3@gmail.com>
2023-05-11 17:35:39 -0300

    Update FE.c

--- FE.c
+++ FE.c
@@ -11,17 +11,17 @@ int queue_empty(Queue *f){
     if(f->tamanho == 0)
         return 1;
     return 0;
-};
+}
 
 void queue_push(Queue *f, int x){
     f->vet[f->fim] = x;
     f->fim = (f->fim+1) % MAX;
     f->tamanho++;
-};
+}
 
 int queue_pop(Queue *f){
     int aux = f->vet[f->inicio];
     f->inicio = (f->inicio + 1) % MAX;
     f->tamanho--;
     return aux;
-};
+}
